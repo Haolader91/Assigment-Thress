bb955ff73ef1aa4f05f91ae5c650303bd91dbcbb
Haolader91 <haolader910@gmail.com>
2026-02-08 01:14:52 +0600

    challenge part

--- problem-one.js
+++ problem-one.js
@@ -6,9 +6,35 @@ function newPrice(currentPrice, discount) {
     const finalPrice = currentPrice - discountAmount;
 
     return finalPrice.toFixed(3);
+
+    // function call code
+    const result = newPrice(1500, 20); 
+    console.log(result);
+ 
 }
-const result = newPrice(1500, 50); 
-console.log(result);
+    // const result = newPrice(1500, 20); 
+    // console.log(result);
+
+
+
+// Challenge part 
+function newPrice(currentPrice, discount) {
+  if (typeof discount !== "number" || typeof currentPrice !== "number" || discount < 0 || discount > 100) {
+    return "Invalid";
+  }
+
+    const discountAmount = currentPrice * discount / 100;
+    const finalPrice = currentPrice - discountAmount;
+
+    return finalPrice.toFixed(3);
+
+    // function call code
+    const result = newPrice("1500", 20);
+    console.log(result);
+
+}
+    // const result = newPrice("1500", 20);
+    // console.log(result);
 
 
 
